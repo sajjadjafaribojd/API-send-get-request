624cc24c34faca6743f0bf64b29993637247bd74
سجاد جعفری <s.jafari@greenweb.local>
2020-07-04 11:37:40 +0430

    add header to request

--- API-send-request.py
+++ API-send-request.py
@@ -3,9 +3,9 @@ import requests
 import json
 
 URL ="http://5.9.6.3./api/v1/suspend"
-#headers = {'token': '586524652kst',"Content-Type":"application/json"}
+headers = {'token': '586524652kst',"Content-Type":"application/json"}
 PARAMS = {'domainName':'prdevops.ir','suspend':'0'} 
 
-r = requests.get(url = URL, params = PARAMS) 
+r = requests.get(url = URL, params = PARAMS,headers=Aheaders) 
 #data = r.json() 
 print(r.text)
