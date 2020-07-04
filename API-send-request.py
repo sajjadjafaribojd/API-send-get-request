f9f3dc90bb4375e2f2c30bbec539e832dd7ad192
سجاد جعفری <s.jafari@greenweb.local>
2020-07-04 11:34:41 +0430

    init commit

---
+++ API-send-request.py
@@ -0,0 +1,11 @@
+# importing the requests library 
+import requests 
+import json
+
+URL ="http://5.9.6.3./api/v1/suspend"
+#headers = {'token': '586524652kst',"Content-Type":"application/json"}
+PARAMS = {'domainName':'prdevops.ir','suspend':'0'} 
+
+r = requests.get(url = URL, params = PARAMS) 
+#data = r.json() 
+print(r.text)
